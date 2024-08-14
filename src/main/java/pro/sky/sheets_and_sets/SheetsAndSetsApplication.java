5adc78477f2e7e6c425257f92aaaf70980e59db6
Artem <artem.yaremenko.1992@gmail.com>
2024-08-14 22:41:58 +0300

    Initial commit

---
+++ src/main/java/pro/sky/sheets_and_sets/SheetsAndSetsApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.sheets_and_sets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SheetsAndSetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SheetsAndSetsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/sheets_and_sets/SheetsAndSetsApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.sheets_and_sets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SheetsAndSetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
